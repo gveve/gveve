5ba41eb6cdb854b6a99a177fe3a4abcedaeb6143
github username <github email address>
2018-03-08 14:30:22 -0500

    dfjknskj

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import '../index.tailwind.css';
 import cart2 from "../images/art/cart2.jpg";
 import gurl from "../images/art/gurl.jpg";
@@ -16,6 +16,8 @@ import sculptlady2 from "../images/art/sculptlady2.jpg";
 
   let arts = [sculpback, sculpside, sculptlady, sculptlady2, nona, poppi, mansculpt, pigskull, cart2, gurl,]
 
+
+
 class Art extends Component{
   constructor(){
     super()
@@ -26,21 +28,31 @@ class Art extends Component{
   }
 
 
-      showImages = () => {
-        return arts.map( (thing, i) => {
-          return (
-          <div className="column nature">
-          <div className="content w-64 mx-3 my-6">
-            <img src={thing} alt="Mountains" style={{width: "100%"}}/>
-          </div>
-          </div>
-        )
-        })
-      }
-
-
 
   render(){
+
+    const images = importAll(require.context('../images/art', false, /\.(png|jpe?g|svg)$/));
+
+    function importAll(r){
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+    }
+
+    function showImages(){
+      return Object.keys(images).map( (thing, i) => {
+        debugger;
+        let other = `../images/art/${thing}`
+        return (
+        <div className="column nature">
+        <div className="content w-64 mx-3 my-6">
+          <img src={require('../images/art/pigskull.jpg')} alt="Mountains" style={{width: "100%"}}/>
+        </div>
+        </div>
+      )
+      })
+    }
+
 
     return(
       <div className="w-full h-full flex overflow-auto">
@@ -48,7 +60,17 @@ class Art extends Component{
           <div className='sm:w-2 xl:w-16 md:w-16 lg:w-16 h-full'/>
           <div className='flex-1 mt-8 text-center'>
               <div className="flex flex-wrap justify-around">
-                {this.showImages()}
+              <div className="column nature">
+              <div className="content w-64 mx-3 my-6">
+                <img src={pigskull} alt="Mountains" style={{width: "100%"}}/>
+              </div>
+              </div>
+              <div className="column nature">
+              <div className="content w-64 mx-3 my-6">
+                <img src={require('../images/art/pigskull.jpg')} alt="Mountains" style={{width: "100%"}}/>
+              </div>
+              </div>
+                {showImages()}
               </div>
           </div>
           <div className='sm:w-2 lg:w-12 xl:w-12 md:w-12 h-full'/>
